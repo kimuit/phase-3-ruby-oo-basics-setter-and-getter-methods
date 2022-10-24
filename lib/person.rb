e65b3c0089db3045a4a48c630cb434494bd08bbe
kimuit117 <kemoikiuita@gmail.com>
2022-10-24 07:34:53 +0300

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
+# getters and setters methods
 
+    def name
+        @name
+    end
+    def name=(name)
+        @name = name
+    end
+
+    def job
+        @job
+    end
+
+    def job=(job)
+        @job = job
+    end
 end
