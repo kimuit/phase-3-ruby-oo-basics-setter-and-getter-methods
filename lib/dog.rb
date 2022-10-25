ae8e02248c5e57553cd5b0d1b0f4f45845b5cc6b
kimuit117 <kemoikiuita@gmail.com>
2022-10-25 15:01:56 +0300

    made changes

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,33 @@
 # getters and setters methods
+# class Dog
+#     def name
+#         @name
+#     end
+
+#     def name=(name)
+#         @name = name
+#     end
+
+#     def breed
+#         @breed
+#     end
+
+#     def breed=(breed)
+#         @breed = breed
+#     end
+
+# end
 class Dog
-    def name
-        @name
-    end
-
-    def name=(name)
-        @name = name
-    end
-
-    def breed
-        @breed
-    end
-
-    def breed=(breed)
-        @breed = breed
-    end
-
+def name
+    @name
+end
+def name=(name)
+    @name=name
+end
+def breed
+    @breed
+end
+def breed=(breed)
+    @breed=breed
+end
 end
